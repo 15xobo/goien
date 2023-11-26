a1ac61d8a5110660d1c46f396eec3de34a3f085d
Bo <15xobo@gmail.com>
2023-11-26 09:41:08 -0800

    Add a new goi page.

---
+++ src/app/goi/new/page.tsx
@@ -0,0 +1,12 @@
+export default function NewGoi() {
+  return (
+    <div>
+      <form>
+        <input type="text" name="text" autoFocus/>
+        <button type="submit">
+          Submit
+        </button> 
+      </form>
+    </div>
+  )
+}
