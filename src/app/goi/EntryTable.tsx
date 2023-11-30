2999b4b52bc6038bc4d98d67f2691c1cf24a1c20
Bo <15xobo@gmail.com>
2023-11-29 17:06:55 -0800

    Add cancel and confirm buttons.

--- src/app/goi/EntryTable.tsx
+++ src/app/goi/EntryTable.tsx
@@ -4,11 +4,13 @@ import { addEntry, deleteEntry } from './actions'
 
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button'
+import CheckIcon from '@mui/icons-material/Check';
 import Checkbox from '@mui/material/Checkbox'
 import ClearIcon from '@mui/icons-material/Clear';
 import { GoiEntry as GoiEntryData } from '@/app/lib/goi'
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper'
+import RemoveIcon from '@mui/icons-material/Remove';
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -22,16 +24,28 @@ function EntryRows({ goiEntries, selectedRow, onRowSelected }: { goiEntries: Arr
     return goiEntries.map((entry, index) => {
         return (
             <TableRow key={index} hover>
-                <TableCell padding="checkbox">
-                    <Checkbox
-                        checked={index === selectedRow}
-                        onChange={(event) => {
-                            if (event.target.checked) {
-                                onRowSelected(index)
-                            } else {
-                                onRowSelected(-1)
-                            }
-                        }} />
+                <TableCell padding="checkbox" sx={selectedRow === index ? {} : { visibility: 'hidden' }}>
+                    <IconButton color='success' >
+                        <CheckIcon />
+                    </IconButton>
+                </TableCell>
+                <TableCell padding="checkbox" sx={true ? {} : { visibility: 'hidden' }}>
+                    {selectedRow === index ? (
+                        <IconButton
+                            color='error' 
+                            onClick={() => onRowSelected(-1)}
+                        >
+                            <ClearIcon />
+                        </IconButton>
+                    ) : (
+                        <IconButton 
+                            disabled={selectedRow !== -1}
+                            color='info' 
+                            onClick={() => onRowSelected(index)}
+                        >
+                            <RemoveIcon />
+                        </IconButton>
+                    )}
                 </TableCell>
                 <TableCell>{entry.word}</TableCell>
                 <TableCell>{entry.sentence}</TableCell>
@@ -46,41 +60,49 @@ function NewEntryRow(
         setEntry: (entry: GoiEntryData | undefined) => void
     }
 ) {
+    const editingActive = goiEntry !== undefined
+
     return (
         <TableRow hover>
+            <TableCell padding="checkbox" sx={editingActive ? {} : { visibility: 'hidden' }}>
+                <IconButton color='success' >
+                    <CheckIcon />
+                </IconButton>
+            </TableCell>
             <TableCell padding="checkbox">
-                {goiEntry ? (
+                {editingActive ? (
                     <IconButton
+                        color='error' 
                         onClick={() => setEntry(undefined)}
                     >
                         <ClearIcon />
                     </IconButton>
                 ) : (
-
                     <IconButton
+                        color='info' 
                         onClick={() => setEntry({ word: '', sentence: '' })}
                     >
                         <AddIcon />
                     </IconButton>
                 )}
             </TableCell>
-            <TableCell sx={goiEntry ? {} : { visibility: 'hidden' }}>
+            <TableCell sx={editingActive ? {} : { visibility: 'hidden' }}>
                 <TextField
                     variant='standard'
                     size='small'
                     fullWidth
                     multiline
-                    value={goiEntry && goiEntry.word}
+                    value={goiEntry?.word}
                     onChange={(event) => setEntry({ word: event.target.value, sentence: goiEntry!.sentence })}
                 />
             </TableCell>
-            <TableCell sx={goiEntry ? {} : { visibility: 'hidden' }}>
+            <TableCell sx={editingActive ? {} : { visibility: 'hidden' }}>
                 <TextField
                     variant='standard'
                     size="small"
                     fullWidth
                     multiline
-                    value={goiEntry && goiEntry.sentence}
+                    value={goiEntry?.sentence}
                     onChange={(event) => setEntry({ word: goiEntry!.word, sentence: event.target.value })}
                 />
             </TableCell>
