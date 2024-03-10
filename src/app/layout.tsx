bda48289a4b02b3b848425647e588b490383cb51
15xobo <15xobo@gmail.com>
2024-03-10 00:30:03 -0800

    Add an app bar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Container from '@mui/material/Container'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AppBar position="sticky">
+          <Container maxWidth="md">
+            <Toolbar>
+              <Button><Link href="/goi/テスト">Wordbook</Link></Button>
+              <Button><Link href="/en/テスト">Card</Link></Button>
+            </Toolbar>
+          </Container>
+        </AppBar>
         <Container maxWidth='md' sx={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           {children}
         </Container>
